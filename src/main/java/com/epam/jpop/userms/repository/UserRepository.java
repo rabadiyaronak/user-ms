6ba016fe833ee6b7154a86e5d4b37de404899e3d
Ronak_Rabadiya <rabadiyaronak@gmail.com>
2021-06-15 21:41:38 +0530

    user-service | refactor package naming

--- src/main/java/com/epam/jpop/userms/repository/UserRepository.java
+++ src/main/java/com/epam/jpop/userms/repository/UserRepository.java
@@ -1,6 +1,6 @@
 package com.epam.jpop.userms.repository;
 
-import com.epam.jpop.userms.model.User;
+import com.epam.jpop.userms.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
