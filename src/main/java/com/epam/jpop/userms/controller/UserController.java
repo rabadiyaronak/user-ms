9271613f4f8f94be2380ed8c620af71a43f47fab
Ronak_Rabadiya <rabadiyaronak@gmail.com>
2021-06-15 22:31:33 +0530

    user-service | integrate controller to business logic

--- src/main/java/com/epam/jpop/userms/controller/UserController.java
+++ src/main/java/com/epam/jpop/userms/controller/UserController.java
@@ -1,39 +1,45 @@
 package com.epam.jpop.userms.controller;
 
 import com.epam.jpop.userms.model.UserDetails;
+import com.epam.jpop.userms.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
 
     @GetMapping
     public List<UserDetails> getAll() {
-        return Collections.emptyList();
+        return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public UserDetails getById(Long id) {
-        return UserDetails.builder().build();
+    public UserDetails getById(@PathVariable Long id) {
+        return userService.getUserById(id);
     }
 
     @PostMapping
-    public ResponseEntity<Void> add(UserDetails userDetails) {
-        return ResponseEntity.created(URI.create("")).build();
+    public ResponseEntity<Void> add(@RequestBody UserDetails userDetails) {
+        UserDetails savedUserDetails = userService.saveUser(userDetails);
+        return ResponseEntity.created(URI.create("/api/v1/users/" + savedUserDetails.getId())).build();
     }
 
-    @PutMapping
-    public UserDetails update(UserDetails userDetails, Long id) {
-        return UserDetails.builder().build();
+    @PutMapping("/{id}")
+    public UserDetails update(@RequestBody UserDetails userDetails, @PathVariable Long id) {
+        return userService.updateUser(userDetails, id);
     }
 
-    @DeleteMapping
-    public void delete(Long id) {
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.deleteUserById(id);
     }
 
 
