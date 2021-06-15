5c1e69819de8711245351292e83f383a1acd4b70
Ronak_Rabadiya <rabadiyaronak@gmail.com>
2021-06-15 11:27:36 +0530

    user-service | initial commit

---
+++ src/test/java/com/epam/jpop/userms/UserMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.jpop.userms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
